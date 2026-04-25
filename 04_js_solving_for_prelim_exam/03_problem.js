118373301d82613943125b4ae423825052c4e84a
mohammedrazaali-1 <mohammedrazaofficial@gmail.com>
2026-04-25 17:20:09 +0530

    solved third prelim exam problem

---
+++ 04_js_solving_for_prelim_exam/03_problem.js
@@ -0,0 +1,31 @@
+//  Write JavaScript Code that elaborate non-primitive data type in 
+// details
+
+// 1) array 
+
+let arr1 = [1,2,3,4,5];
+console.log("array is : ", arr1 );
+
+//2) object 
+
+let myobject = {
+    name : "raza",
+    age :21,
+    college : "IICMR"
+}
+console.log(myobject)
+
+// 3) function 
+
+function function_type(){
+    let func_name="raza ali";
+    let func_age = 22;
+    let func_clg = "IICMR nigdi"
+    console.log( `Name is ${func_name} , age is ${func_age} and college is ${func_clg}`)
+}
+
+
+function_type()
+
+// 4) regEx 
+let myregEx =/abcd/;

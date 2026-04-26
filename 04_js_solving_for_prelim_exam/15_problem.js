2a8cd74428407f2e56746af8b42bbf7bf5b5c40f
mohammedrazaali-1 <mohammedrazaofficial@gmail.com>
2026-04-26 21:08:35 +0530

    solved 15th prelim exam problem

---
+++ 04_js_solving_for_prelim_exam/15_problem.js
@@ -0,0 +1,6 @@
+// program to reverse a string
+
+let string = "hello i am RAZA";
+let reverse_string = string.split('').reverse().join('');
+console.log(`reverse of ${string} is : ${reverse_string}`);
+

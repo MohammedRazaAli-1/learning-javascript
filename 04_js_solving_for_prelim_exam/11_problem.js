039394fcac0415c9e89437d5f65efcdff6417431
mohammedrazaali-1 <mohammedrazaofficial@gmail.com>
2026-04-26 20:32:43 +0530

    solved 11th prelim exam problem

---
+++ 04_js_solving_for_prelim_exam/11_problem.js
@@ -0,0 +1,9 @@
+//  program to display the sum of natural numbers 
+
+let range = 130;
+let sum = 0;
+for(let i = 1; i<=range; i++){
+    sum = sum + i;
+    
+}
+console.log(`the sum on ${range} natural number is ${sum}`);

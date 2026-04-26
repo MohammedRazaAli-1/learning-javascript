33cd706de587da0137004f071b2af0088abd2a16
mohammedrazaali-1 <mohammedrazaofficial@gmail.com>
2026-04-26 21:21:09 +0530

    solved 17th prelim exam problem

---
+++ 04_js_solving_for_prelim_exam/17_problem.js
@@ -0,0 +1,7 @@
+//  Separating the words based on spaces and then counting the number 
+// of words. 
+
+let string = "kaay bhaiyya kaay mahanto , brr aisa na ...   ";
+let totalWordsLength = string.split(' ').length;
+console.log(string.split(' '));
+console.log(totalWordsLength);
